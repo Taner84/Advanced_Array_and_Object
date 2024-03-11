49939cbb593c3692c31f22f078ce84991185d664
Taner <taner@taner-k.nl>
2024-03-11 12:33:36 +0000

    letter counter added

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const sumArray = (numbers) => {
+const letterCounter = () => {
+
+
+  //counter
   let result = 0;
 
-  for (const number of numbers) {
-    //console.log(numbers);
-    result = result + number;
+  for (const index in phrase) {
+    console.log(Number(index) + 1)
+    result = Number(index) + 1
   }
-  return { result } 
+
+  // return { result:  phrase.length }
+  return { result }
 }
 
-const nums = [1,2,3,4,5]
-console.log(sumArray(nums))
+// const phrase = prompt('Write your phrase.')
+
+const phrase = ('Write your phrase.')
+console.log(letterCounter(phrase))
+
+// const sumArray = (numbers) => {
+//   let result = 0;
+
+//   for (const number of numbers) {
+//     //console.log(numbers);
+//     result = result + number;
+//   }
+//   return { result } 
+// }
+
+// const nums = [1,2,3,4,5]
+// console.log(sumArray(nums))
+
+//letter counter
+
